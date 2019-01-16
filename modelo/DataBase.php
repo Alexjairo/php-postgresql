856de14fe7583575656bb272ca45b7b08b446804
Alexjairo <jairomorocho5@gmail.com>
2019-01-16 15:57:05 -0500

    primeros cambios

---
+++ modelo/DataBase.php
@@ -0,0 +1,23 @@
+<?php
+class DataBase{
+ public function insertarProducto($nombre,$descripcion,$categoria,$precio){
+
+
+
+     $modelo= new Conexion();
+     $Conexion=$modelo-> get_conexion();
+     $sql= "insert into Producto(nombre, descripcion, categoria, precio) values(:nombre, :descripcion, :categoria, :precio)";
+     $statement = $Conexion->prepare($sql);
+     $statement -> bindParam(':nombre',$nombre);
+     $statement-> bindParam(':descripcion',$descripcion);
+     $statement-> bindParam(':categoria',$categoria);
+     $statement-> bindParam(':precio',$precio);
+     if(!$statement){
+         return "Error no se puede realizar la carga";
+     }else{
+         $statement-> execute();
+         return "la insercion se realizó con exito";
+     }
+ }
+}
+?>
