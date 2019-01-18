a12ef464520741a6b8a76b297ca2b4a585bd5763
Alexjairo <jairomorocho5@gmail.com>
2019-01-18 10:40:26 -0500

    Gitacora saved 1 changed files

--- modelo/DataBase.php
+++ modelo/DataBase.php
@@ -26,7 +26,7 @@ class DataBase{
          $modelo= new Conexion();
          $conexion =$modelo -> get_conexion();
          $sql=" select * from producto";
-         $statement = $Conexion->prepae(sql);
+         $statement = $Conexion->prepare(sql);
        
      
      }
