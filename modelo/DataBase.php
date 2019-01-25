29664cd3d49292f3a57ce4c265c18221f0f86703
Alexjairo <jairomorocho5@gmail.com>
2019-01-24 23:40:50 -0500

    Gitacora saved 1 changed files

--- modelo/DataBase.php
+++ modelo/DataBase.php
@@ -58,7 +58,7 @@ class DataBase{
         $Conexion =$modelo -> get_conexion();
         $nombre="%".$nombre."%";
         $sql=" select * from producto where nombre like :nombre order by id";
-        $statement = $Conexion->prepare($sql);
+        $statement = $conexion->prepare($sql);
         $statement->bindParam('nombre',$nombre);
         $statement -> execute();
         while ($resultado=$statement->fetch()){
