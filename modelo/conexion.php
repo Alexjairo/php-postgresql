a93c411f12ba3bf932736c3406a21dce244bdcaf
Alexjairo <jairomorocho5@gmail.com>
2019-01-28 15:57:20 -0500

    Gitacora saved 1 changed files

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,7 +3,7 @@ class Conexion{
     public function get_conexion() {
         $user="postgres";
         $pass="pokemongo99";
-        $host="localhost";
+        $host="localhostq";
         $db="pedido";
         $conexion= new PDO("pgsql:host=$host;dbname=$db",$user,$pass);
     return $conexion;
